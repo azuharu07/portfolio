b7787a7a87da79be474e4e3068e90753ae20aa59
azuharu07 <azuha07i@gmail.com>
2020-08-29 14:17:54 +0900

    Buefyとvue-smoothscroll導入

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import Buefy from "buefy";
+import smoothScroll from "vue-smoothscroll";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(Buefy);
+Vue.use(smoothScroll);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+    render: (h) => h(App),
+}).$mount("#app");
