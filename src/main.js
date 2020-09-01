1af33e0f70fc1ba19f8f3251f31e0701d7972171
azuharu07 <azuha07i@gmail.com>
2020-09-01 23:50:36 +0900

    スムーススクロールを実現

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
 import Buefy from "buefy";
-import smoothScroll from "vue-smoothscroll";
 
 Vue.config.productionTip = false;
 Vue.use(Buefy);
-Vue.use(smoothScroll);
 
 new Vue({
     render: (h) => h(App),
