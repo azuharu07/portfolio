54b2b9a126364200428e3124f69a24a8f48f4520
azuharu07 <azuha07i@gmail.com>
2020-09-05 18:26:31 +0900

    Contactを作成

--- src/components/contents/Contact.vue
+++ src/components/contents/Contact.vue
@@ -2,7 +2,14 @@
     <ContentComponent title="Contact">
         <template>
             <div class="contact">
-                <div class="contents">aaa</div>
+                <a class="contact__link" href="https://github.com/azuharu07">
+                    <img src="@/assets/images/github-logo.png" width="60px" alt="github-logo" />
+                    <label>GitHub</label>
+                </a>
+                <a class="contact__link" href="https://twitter.com/azuharu07">
+                    <img src="@/assets/images/twitter-logo.png" width="60px" alt="github-logo" />
+                    <label>Twitter</label>
+                </a>
             </div>
         </template>
     </ContentComponent>
@@ -17,4 +24,15 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.contact {
+    display: flex;
+    justify-content: center;
+    &__link {
+        margin: 0 1em;
+        display: flex;
+        flex-direction: column;
+        color: black;
+    }
+}
+</style>
