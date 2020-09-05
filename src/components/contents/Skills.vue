1d8322e9e1a687c2652f60560e234b1334bf126a
azuharu07 <azuha07i@gmail.com>
2020-09-06 04:59:11 +0900

    とりあえずスキルエリア追加

--- src/components/contents/Skills.vue
+++ src/components/contents/Skills.vue
@@ -1,8 +1,17 @@
 <template>
     <ContentComponent title="Skills">
         <template>
-            <div class="skills">
-                <div class="contents">aaa</div>
+            <div class="tile is-vertical">
+                <div class="tile is-horizontal">
+                    <SkillCard title="JavaScript" level="4.5"></SkillCard>
+                    <SkillCard title="PHP" level="4"></SkillCard>
+                    <SkillCard title="Java" level="3.5"></SkillCard>
+                </div>
+                <div class="tile is-horizontal">
+                    <SkillCard title="Python" level="2"></SkillCard>
+                    <SkillCard title="SQL" level="3.5"></SkillCard>
+                    <SkillCard title="Vue.js" level="4"></SkillCard>
+                </div>
             </div>
         </template>
     </ContentComponent>
@@ -10,10 +19,11 @@
 
 <script>
 import ContentComponent from "@/components/items/ContentComponent.vue";
+import SkillCard from "@/components/items/SkillCard.vue";
 
 export default {
     name: "Skills",
-    components: { ContentComponent },
+    components: { ContentComponent, SkillCard },
 };
 </script>
 
---
+++ src/components/items/SkillCard.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="card skill-card">
+        <header class="card-header">
+            <img :src="image" />
+            <p class="card-header-title skill-card__title">
+                {{ title }}
+            </p>
+        </header>
+        <div class="card-content">
+            <StarRating
+                :item-size="25"
+                :show-rating="false"
+                :increment="0.5"
+                :rating="level"
+                :read-only="true"
+            ></StarRating>
+        </div>
+    </div>
+</template>
+
+<script>
+import { StarRating } from "vue-rate-it";
+
+export default {
+    name: "SkillCard",
+    components: {
+        StarRating,
+    },
+    props: {
+        title: {
+            type: String,
+            default: "",
+        },
+        level: {
+            type: Number,
+            default: 0,
+        },
+    },
+};
+</script>
+
+<style lang="scss">
+.skill-card {
+    margin: 1em auto;
+
+    &__title {
+        justify-content: center;
+    }
+}
+</style>
